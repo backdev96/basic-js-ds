287fb6593fc964a132c595d59a47af966c03910a
Stas <stasefremovx@gmail.com>
2023-04-17 23:59:31 +0200

    added binary search

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,47 +1,109 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+		this.root = null
+	}
 
-  root() {
+	root() {
+		if (!this.root) {
+			return null
+		}
+
+		return this.root
+	}
+
+	add(data) {
+		if (!this.root) {
+			this.root = new Node(data)
+		} else {
+			this.addTo(this.root, data)
+		}
+	}
+
+	addTo(node, value) {
+		if (value < node.data) {
+			if (!node.left) {
+				node.left = new Node(value)
+			} else {
+				this.addTo(node.left, value)
+			}
+		} else {
+			if (!node.right) {
+				node.right = new Node(value)
+			} else {
+				this.addTo(node.right, value)
+			}
+		}
+	}
+
+	has(data) {
+		return !!this.find(data)
+	}
+
+	find(data) {
+		if (!this.root || !this.root.data) {
+			return null
+		}
+
+		return this.findWithParent(this.root, data)
+	}
+
+	findWithParent(node, value) {
+		if (node.data === value) {
+			return node
+		}
+
+		if (value < node.data) {
+			if (!node.left) {
+				return null
+			}
+
+			return this.findWithParent(node.left, value)
+		} else {
+			if (!node.right) {
+				return null
+			}
+
+			return this.findWithParent(node.right, value)
+		}
+	}
+
+	remove(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	min() {
+		const arr = this.convertToArray(this.root)
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		return Math.min(...arr)
+	}
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	max() {
+		const arr = this.convertToArray(this.root)
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		return Math.max(...arr)
+	}
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	convertToArray(node) {
+		if (!node) {
+			return []
+		}
+
+		return [
+			node.data,
+			this.convertToArray(node.left),
+			this.convertToArray(node.right),
+		].flat(Infinity)
+	}
 }
 
 module.exports = {
