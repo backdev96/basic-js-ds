d32282e7f540ad600cb596363f7f29a767998d90
Stas <stasefremovx@gmail.com>
2023-04-18 00:17:06 +0200

    added stack

--- src/stack.js
+++ src/stack.js
@@ -13,10 +13,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  push(element){
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
+}
 
   pop()
   {
@@ -28,9 +28,11 @@ class Stack {
       return this.items.pop();
   }
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  peek()
+  {
+      // return the top most element from the stack
+      // but does'nt delete it.
+      return this.items[this.items.length - 1];
   }
 }
 
